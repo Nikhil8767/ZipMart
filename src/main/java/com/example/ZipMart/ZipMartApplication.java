eb5011edf8c031d86838d9df162fb1220115a0d1
Nikhil Gupta <nikhilgupta932003@gmail.com>
2026-05-22 23:10:21 +0530

    added the structure and code

---
+++ src/main/java/com/example/ZipMart/ZipMartApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZipMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipMartApplication.class, args);
+	}
+
+}
